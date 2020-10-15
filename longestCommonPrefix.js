7a192410930eaf4fb9493e356fb324a913880db5
Micah Peterson <micahpeterson@live.com>
2020-10-14 20:03:38 -0500

    first round of testing ready

--- longestCommonPrefix.js
+++ longestCommonPrefix.js
@@ -2,7 +2,10 @@
 
 const longestCommonPrefix = function(strs) {
     
+    // the variable we are gonna push common prefixes to
     let result = "";
+
+    // the 2D array we are gonna push the array of letters to
     let twoDeeArray = [];
     
     strs.forEach(item => {
@@ -15,7 +18,7 @@ const longestCommonPrefix = function(strs) {
 
         for(let i = 0; i < array.length; i++)
         {
-            for(let j = [0] j < twoDeeArray.length; j++)
+            for(let j = [0]; j < twoDeeArray.length; j++)
             {
                 if(twoDeeArray[i][j] === twoDeeArray[i+1][j])
                 {
