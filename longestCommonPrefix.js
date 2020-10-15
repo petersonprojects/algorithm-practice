8ac28804611b66aa786f38018dbd22f2c207c990
Micah Peterson <micahpeterson@live.com>
2020-10-14 20:02:12 -0500

    javascript 2d array method

--- longestCommonPrefix.js
+++ longestCommonPrefix.js
@@ -8,15 +8,22 @@ const longestCommonPrefix = function(strs) {
     strs.forEach(item => {
 
         let array = item.split('');
-        
+
         twoDeeArray.push(array);
 
         console.log(twoDeeArray)
 
-        // for(let i = 0; i < array.length; i++)
-        // {
-        //     if(array[i])
-        // }
+        for(let i = 0; i < array.length; i++)
+        {
+            for(let j = [0] j < twoDeeArray.length; j++)
+            {
+                if(twoDeeArray[i][j] === twoDeeArray[i+1][j])
+                {
+                    result = result + twoDeeArray[i][j]
+                    console.log(result)
+                }
+            }
+        }
     })
     
     return result;
